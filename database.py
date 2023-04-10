ed6a671746489089a28d4820cfca05a4224fbf1f
Shashank Prakash <31253156+quickSilverShanks@users.noreply.github.com>
2023-04-10 20:42:41 +0000

    added new sql connection string

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, text
 import os
 
-db_connection_string = os.environ['DB_CONNECTION_STRING']
+db_connection_string = os.environ['DB_CONNECTION_STRING_NEW']
 
 engine = create_engine(
   db_connection_string, 
